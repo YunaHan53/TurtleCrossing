562126bb52db065f50fba27f6f7b54593b27fe45
YunaH <Yunahan53@gmail.com>
2026-01-19 22:52:25 -0500

    Created a new turtle player that starts from the bottom and goes up by using the Up arrow key.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -8,7 +8,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+# Turtle movement
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
