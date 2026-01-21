400b2afc78656bdf3efe1c97b2b2565565aaea4a
YunaH <Yunahan53@gmail.com>
2026-01-20 20:56:58 -0500

    Added methods to increase the level number on the screen for the player.

--- scoreboard.py
+++ scoreboard.py
@@ -11,9 +11,17 @@ class Scoreboard(Turtle):
         self.penup()
         self.color('black')
         self.goto(-200, 250)
-        self.write(f"Level: {self.level}", align="center", font=FONT)
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align="center", font=FONT)
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="center", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
 
